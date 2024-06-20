936b38afd02b078ad08c2a5b44fda89401a49cdf
Deep-Inexture <deeppanchal@inexture.com>
2024-06-20 11:35:45 +0530

    Modified BatchConfig File , Injected JobRepository and TransactionManager beans using Constructor Injection

--- src/main/java/com/springbatchprocessing/config/BatchConfig.java
+++ src/main/java/com/springbatchprocessing/config/BatchConfig.java
@@ -5,55 +5,52 @@ import com.springbatchprocessing.itemprocessor.CustomerItemProcessor;
 import com.springbatchprocessing.itemreader.CustomerItemReader;
 import com.springbatchprocessing.itemwriter.CustomerItemWriter;
 import com.springbatchprocessing.tasklet.DataCleansingTasklet;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
-
+import org.springframework.transaction.PlatformTransactionManager;
 import javax.sql.DataSource;
 
 @Configuration
-@EnableBatchProcessing
+@Slf4j
 public class BatchConfig {
 
     private final DataCleansingTasklet dataCleansingTasklet;
-
-    private final JobBuilderFactory jobBuilderFactory;
-
-    private final StepBuilderFactory stepBuilderFactory;
-
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
     private final DataSource dataSource;
 
-    public BatchConfig(DataCleansingTasklet dataCleansingTasklet, JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, DataSource dataSource) {
+    public BatchConfig(DataCleansingTasklet dataCleansingTasklet, JobRepository jobRepository, PlatformTransactionManager transactionManager, DataSource dataSource) {
         this.dataCleansingTasklet = dataCleansingTasklet;
-        this.jobBuilderFactory = jobBuilderFactory;
-        this.stepBuilderFactory = stepBuilderFactory;
+        this.jobRepository = jobRepository;
+        this.transactionManager = transactionManager;
         this.dataSource = dataSource;
     }
 
     @Bean
-    public Job customerJob(){
-        return jobBuilderFactory.get("customer-job")
+    public Job customerJob() {
+        return new JobBuilder("customer-job", jobRepository)
                 .start(customerStep())
                 .next(dataCleansingStep())
                 .build();
     }
 
     @Bean
-    public Step dataCleansingStep(){
-        return stepBuilderFactory.get("dataCleansingTasklet")
-                .tasklet(dataCleansingTasklet)
+    public Step dataCleansingStep() {
+        return new StepBuilder("dataCleansingStep", jobRepository)
+                .tasklet(dataCleansingTasklet, transactionManager)
                 .build();
     }
 
     @Bean
-    public Step customerStep(){
-        return stepBuilderFactory.get("customer-step")
-                .<Customer,Customer>chunk(100)
+    public Step customerStep() {
+        return new StepBuilder("customerStep", jobRepository)
+                .<Customer, Customer>chunk(100, transactionManager)
                 .reader(new CustomerItemReader().customerFlatFileItemReader())
                 .processor(new CustomerItemProcessor())
                 .writer(new CustomerItemWriter(new JdbcTemplate(dataSource)))
