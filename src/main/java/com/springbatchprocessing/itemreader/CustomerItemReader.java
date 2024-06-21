82ccf6f03f7a7c69fb3ef80a9df2db36885eaad9
Deep-Inexture <deeppanchal@inexture.com>
2024-06-21 14:30:15 +0530

    Modified CustomerItemReader,Added MultiResourceItemReader<Customer> bean method to CustomerItemReader class

--- src/main/java/com/springbatchprocessing/itemreader/CustomerItemReader.java
+++ src/main/java/com/springbatchprocessing/itemreader/CustomerItemReader.java
@@ -3,20 +3,34 @@ package com.springbatchprocessing.itemreader;
 import com.springbatchprocessing.entity.Customer;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.MultiResourceItemReader;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomerItemReader {
 
+    @Value("classpath:csv/myrecords_*.csv")
+    private Resource[] resources;
+
+
+    @Bean
+    public MultiResourceItemReader<Customer> reader(FlatFileItemReader<Customer> customerFlatFileItemReader) {
+        MultiResourceItemReader<Customer> reader = new MultiResourceItemReader<>();
+        reader.setResources(resources);
+        reader.setDelegate(customerFlatFileItemReader);
+        return reader;
+    }
+
     @Bean
     public FlatFileItemReader<Customer> customerFlatFileItemReader() {
         FlatFileItemReader<Customer> reader = new FlatFileItemReader<>();
-        reader.setResource(new ClassPathResource("myrecords.csv"));
+        //        reader.setResource(new ClassPathResource("myrecords_1.csv"));
         reader.setName("csv-reader");
         reader.setLinesToSkip(1);
         reader.setLineMapper(customerLineMapper());
