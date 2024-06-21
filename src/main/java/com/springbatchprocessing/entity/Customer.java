3d98a243adfe24ec34e468fb148ffd460593f755
Deep-Inexture <deeppanchal@inexture.com>
2024-06-21 18:04:52 +0530

    Modified Customer Entity Class , which implements ResourceAware to set Resource

--- src/main/java/com/springbatchprocessing/entity/Customer.java
+++ src/main/java/com/springbatchprocessing/entity/Customer.java
@@ -3,15 +3,25 @@ package com.springbatchprocessing.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.springframework.batch.item.ResourceAware;
+import org.springframework.core.io.Resource;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Customer {
+public class Customer implements ResourceAware {
 
     private Long id;
     private String firstName;
     private String lastName;
     private String email;
     private String profession;
+
+    private String resourceFileName;
+
+    @Override
+    public void setResource(@NonNull Resource resource) {
+        this.resourceFileName = resource.getFilename();
+    }
 }
